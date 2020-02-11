cc116d2ebc1e0f51c6e52830705e8d47a15e3d12
Francois Godin <francois.h.godin@gmail.com>
2020-02-11 10:51:01 +0100

    Initial application commit

---
+++ src/main/java/com/example/SN_SPRING_F_API/SnSpringFApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.SN_SPRING_F_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnSpringFApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnSpringFApiApplication.class, args);
+		System.out.println("Hellow world!");
+	}
+
+}
---
+++ src/test/java/com/example/SN_SPRING_F_API/SnSpringFApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SN_SPRING_F_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnSpringFApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
